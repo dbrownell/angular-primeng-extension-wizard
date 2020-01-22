97096fb1c97a699b6d181e2264fb85ed0f2d0ff0
dbrownell <danbrownell@comcast.net>
2020-01-22 06:55:37 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import {Component} from '@angular/core';
+import {Message} from 'primeng/components/common/api';
 import { AppComponent } from './app.component';
 
 
