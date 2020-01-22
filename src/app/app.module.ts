9ae134a085dc3244a6746ad40770b934d01e3b60
dbrownell <danbrownell@comcast.net>
2020-01-22 07:11:20 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import {Component} from '@angular/core';
 import {Message} from 'primeng/components/common/api';
 import { AppComponent } from './app.component';
 import { UserDataWizardComponent } from './user-data-wizard/user-data-wizard.component';
+import {WizardModule} from 'primeng-extensions-wizard/components/wizard.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, WizardModule ],
   declarations: [ AppComponent, UserDataWizardComponent ],
   bootstrap:    [ AppComponent ]
 })
