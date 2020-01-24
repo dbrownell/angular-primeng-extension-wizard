7546ee63bc215fa9acfa118d5ccd354839f58627
dbrownell <danbrownell@comcast.net>
2020-01-24 08:15:40 -0500

    1 file modified

--- src/app/user-data-wizard/user-data-wizard.component.ts
+++ src/app/user-data-wizard/user-data-wizard.component.ts
@@ -29,34 +29,36 @@ export class UserDataWizardComponent implements OnInit {
        this.messageService.add({severity:'success', summary:'Success Message', detail:'Order submitted'});
     }
 
-    showInfo() {       
-        this.messageService.add({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
+    showInfo() {   
+      this.messageService.clear();    
+      this.messageService.add({severity:'info', summary:'Info Message', detail:'PrimeNG rocks'});
     }
 
     showWarn() {
-        
-        this.messageService.add({severity:'warn', summary:'Warn Message', detail:'There are unsaved changes'});
+      this.messageService.clear();
+      this.messageService.add({severity:'warn', summary:'Warn Message', detail:'There are unsaved changes'});
     }
 
     showError() {
-        this.msgs = [];
-        this.msgs.push({severity:'error', summary:'Error Message', detail:'Validation failed'});
+      this.messageService.clear();
+      this.messageService.add({severity:'error', summary:'Error Message', detail:'Validation failed'});
     }
 
     showMultiple() {
-        
-        this.messageService.addAll([{severity:'info', summary:'Message 1', detail:'PrimeNG rocks'},
-        {severity:'info', summary:'Message 2', detail:'PrimeUI rocks'},
-        {severity:'info', summary:'Message 3', detail:'PrimeFaces rocks'}]);
+      this.messageService.clear();
+      this.messageService.addAll([{severity:'info', summary:'Message 1', detail:'PrimeNG rocks'},
+      {severity:'info', summary:'Message 2', detail:'PrimeUI rocks'},
+      {severity:'info', summary:'Message 3', detail:'PrimeFaces rocks'}]);
     }
     
     showViaService() {
-        this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
+      this.messageService.clear();
+      this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
     }
 
       
-  clear() {
+    clear() {
       this.messageService.clear();
-  }
+    }
 
 }
