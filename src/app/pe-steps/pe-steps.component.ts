ff195cbeaf495233bc3b93f4e74299b7a1e25665
dbrownell <danbrownell@comcast.net>
2020-01-28 08:44:47 -0500

    1 file modified

--- src/app/pe-steps/pe-steps.component.ts
+++ src/app/pe-steps/pe-steps.component.ts
@@ -34,28 +34,28 @@ export class PeStepsComponent implements AfterContentInit, OnChanges, OnInit {
   }
   ngOnInit(){
      this.items = [{
-                label: 'Personal',
+                label: 'Home Furnishing',
                 command: (event: any) => {
                     this.activeIndex = 0;
                     this.messageService.add({severity:'info', summary:'First Step', detail: event.item.label});
                 }
             },
             {
-                label: 'Seat',
+                label: 'Home Accessories',
                 command: (event: any) => {
                     this.activeIndex = 1;
                     this.messageService.add({severity:'info', summary:'Seat Selection', detail: event.item.label});
                 }
             },
             {
-                label: 'Payment',
+                label: 'Cooking & Dinner',
                 command: (event: any) => {
                     this.activeIndex = 2;
                     this.messageService.add({severity:'info', summary:'Pay with CC', detail: event.item.label});
                 }
             },
             {
-                label: 'Confirmation',
+                label: 'Bed & Bath',
                 command: (event: any) => {
                     this.activeIndex = 3;
                     this.messageService.add({severity:'info', summary:'Last Step', detail: event.item.label});
